f896d2c3ab4d61c6b760e0cd42642d4369adc0fe
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-04-24 15:54:24 +0100

    Update arrays

--- 01-essentials/arrays.ts
+++ 01-essentials/arrays.ts
@@ -4,3 +4,6 @@ let foods: Array<string> = ["Pizza", "Chocolate"];
 
 // Both ways of typing are fine and are directly equivalent
 // Either <type>[] or Array<type>
+
+// Remember we can use type inference for the above examples
+// We have just explicity typed to demonstrate syntax
