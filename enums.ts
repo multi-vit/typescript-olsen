79f342d018a0aeaf8125546877494839afa5ee85
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-04-24 14:11:59 +0100

    Add enums

---
+++ enums.ts
@@ -0,0 +1,19 @@
+enum Color {
+  R = 1,
+  G,
+  B,
+}
+let c: Color = Color.R;
+
+console.log(c);
+console.log(Color.G);
+console.log(Color.B);
+
+// This is an example of a numeric enum
+// We have initialised the first value with a number, but you don't have to
+// Following members are auto-incremented (so Color.G == 2, Color.B == 3)
+// You can also leave it blank and first value will be 0 by default
+
+// You can also have String enums
+// They are not auto-incremented
+// Can be useful for debugging as they can describe meaning of their value over plain numbers
