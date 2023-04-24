c10d1ce4fd95604783bff53b4acd0e0b84fe3be2
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-04-24 15:53:36 +0100

    Add arrays

---
+++ 01-essentials/arrays.ts
@@ -0,0 +1,6 @@
+let numbers: number[] = [1, 2];
+// or
+let foods: Array<string> = ["Pizza", "Chocolate"];
+
+// Both ways of typing are fine and are directly equivalent
+// Either <type>[] or Array<type>
