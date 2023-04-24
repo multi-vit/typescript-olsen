35e16880c68c00136fd19a9fb90e6aa8fe70cac8
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-04-24 14:14:57 +0100

    Update tuples to mention mutability

--- tuples.ts
+++ tuples.ts
@@ -1,11 +1,14 @@
-let bd: [number, string];
+let birthday: [number, string];
 
-bd = [3, "December"];
+birthday = [3, "December"];
 
-let day: number = bd[0];
-let month: string = bd[1];
+let day: number = birthday[0];
+let month: string = birthday[1];
 
 // If we didn't explicitly type on line 1 and initialised there
 // TS would infer an array where elements are either strings or numbers
 // Instead of a tuple
 // We don't need to explicitly type on lines 5 and 6
+
+birthday[0] = 12;
+// N.B. Tuples are mutable here!!! (Unlike other languages)
