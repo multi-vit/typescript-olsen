4f635f13bc06473f8b5cd8496dd3a3650f7c9c80
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-04-25 15:55:11 +0100

    Update constructor

--- 03-classes/constructor.ts
+++ 03-classes/constructor.ts
@@ -4,14 +4,17 @@
 // Automatically invoked when you create an object
 
 class Employee {
+  // If you were to initialise values here, they would be immediately overwritten by the constructor
   name: string;
   salary: number;
 
   constructor(name: string, salary: number) {
-    this.name = name;
+    this.name = name; // Must use "this" to access members
     this.salary = salary;
   }
 }
 
 let employeeTwo = new Employee("Marley", 43000);
 console.log(`${employeeTwo.name} earns ${employeeTwo.salary}`);
+
+// You can combine using the constructor to set certain values upon construction and having some values set by default
