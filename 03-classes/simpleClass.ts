e36baf197809bcbef69c12dc894f18299c0c8ce6
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-04-25 14:39:03 +0100

    Start classes learning

---
+++ 03-classes/simpleClass.ts
@@ -0,0 +1,20 @@
+// Just like JavaScript can do OOP, TypeScript extends on this
+// Use the class keyword - convention is for PascalCase for the ClassName
+// Define members using familiar OO syntax
+
+class Employee {
+  name = "";
+  salary = -1;
+}
+
+// You can create and use objects using familar JavaScript syntax
+
+let employeeOne = new Employee();
+
+employeeOne.name = "Scrooge";
+employeeOne.salary = 42000;
+
+console.log(`${employeeOne.name} earns ${employeeOne.salary}`);
+
+// Members are public by default which is why we can access them here
+// Can use the private or protected keywords to avoid this
