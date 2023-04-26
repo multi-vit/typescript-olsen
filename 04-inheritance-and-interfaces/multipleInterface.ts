a188ce825334b4788db306deb520cb489de9a677
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-04-26 10:48:55 +0100

    Updated multiple interfaces to include object literal

--- 04-inheritance-and-interfaces/multipleInterface.ts
+++ 04-inheritance-and-interfaces/multipleInterface.ts
@@ -20,5 +20,19 @@ class MultipleInterfaceClass implements LoggableInterface, Serialisable {
 }
 
 let newMultipleInterfaceClass = new MultipleInterfaceClass();
-newMultipleInterfaceClass.log("Hello world!");
+newMultipleInterfaceClass.log("Hello from MultipleInterfaceClass!");
 newMultipleInterfaceClass.serialise();
+
+// Can also be done with object literal as before:
+
+let objectWithMultipleInterfaces = {
+  log(msg: string) {
+    console.log(`Object literal is logging message: ${msg}`);
+  },
+  serialise() {
+    console.log(`Object literal is serialising`);
+  },
+};
+
+objectWithMultipleInterfaces.log("Hello from Object Literal!");
+objectWithMultipleInterfaces.serialise();
